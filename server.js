bc59ed210877f92f6791c2f6ab660468029dcb58
excelanand <anandkmr.1993@gmail.com>
2016-09-25 11:22:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/Article-one', function (req, res) {
-  res.send("Article 1 was requested and would be served here");
+  res.sendFile(path.join(__dirname, 'ui', 'Article-one.html'));
 });
 
 app.get('/Article-two', function (req, res) {
