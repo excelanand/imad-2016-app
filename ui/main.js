7dae15625e00e2cd8727e531ca5fb762237db95f
excelanand <anandkmr.1993@gmail.com>
2016-09-28 10:59:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,12 @@
 console.log('Loaded!');
 var img = document.getElementById("MAHI");
+var marginLeft = 0;
+function moveRight = ()
+{
+    marginLeft = marginLeft + 5;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick = function ()
 {
-    img.style.marginLeft = '400px';
+    var interval = setInterval(moveRight, 50);
 }
