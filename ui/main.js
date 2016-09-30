acd8f75b00dede0ec7eae0b0524053fe23b2addc
excelanand <anandkmr.1993@gmail.com>
2016-09-30 20:57:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,6 +13,8 @@ img.onclick = function ()
 var button = document.getElementById('counter');
 var counter = 0;
 button.onclick = function() {
+    var request = new XMLHttpRequest();
+    
     counter = counter + 1;
     var span = document.getElementById('count');
     span.innerHTML = counter.toString();
