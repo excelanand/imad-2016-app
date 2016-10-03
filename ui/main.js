baad423cbe33cb1b7509659a04edc2d6e08f120d
excelanand <anandkmr.1993@gmail.com>
2016-10-03 09:46:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -31,12 +31,24 @@ var nameinput = document.getElementById('name');
 var name = nameinput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
-    var names = ['JOHN','JACK','JILL'];
-    var list = '';
-    for(var i=0; i<names.length; i++)
-    {
-        list += '<li>' + names[i] + '</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readystate === XMLHttpRequest.DONE)
+        {
+            if(request.status === 200)
+            {
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                for(var i=0; i<names.length; i++)
+                {
+                    list += '<li>' + names[i] + '</li>';
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
+            }
+        }
+    };
+    request.open('GET','http://excelanand.imad.hasura-app.io/submit?name=' + name,true);
+    request.send(null);
 };
