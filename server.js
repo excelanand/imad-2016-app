3f78022954bb34ed3b503b5ac1945969944771fb
excelanand <anandkmr.1993@gmail.com>
2016-10-03 09:37:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -101,8 +101,8 @@ app.get('/ui/main.js', function (req, res) {
 });
 
 var names = [];
-app.get('/submit/:name', function (req, res) {
-  var name = req.params.name;
+app.get('/submit', function (req, res) {
+  var name = req.query.name;
   names.push(name);
   res.send(JSON.stringify(names));
 });
