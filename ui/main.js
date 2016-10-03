d8b57e2f3accd87dc7fc85b97fcc54cc0aa6c645
excelanand <anandkmr.1993@gmail.com>
2016-10-03 09:20:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,11 +19,24 @@ button.onclick = function() {
             if(request.status === 200)
             {
                 var counter = request.responseText;
-                var span = document.getElementById("count");
+                var span = document.getElementById('count');
                 span.innerHTML = counter.toString();
             }
         }
     };
   request.open('GET','http://excelanand.imad.hasura-app.io/counter',true);
   request.send(null);
+};
+var nameinput = document.getElementById('name');
+var name = nameinput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function() {
+    var names = ['JOHN','JACK','JILL'];
+    var list = '';
+    for(var i=0; i<names.length; i++)
+    {
+        list += '<li>' + names[i] + '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
 };
